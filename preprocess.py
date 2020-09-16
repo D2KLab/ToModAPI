8493d3ca31ab14ed4e959296e760a3743d792a7a
Pasquale Lisena <pasquale.lisena@eurecom.fr>
2020-09-16 16:26:27 +0200

    fix preprocessing

--- preprocess.py
+++ preprocess.py
@@ -4,7 +4,7 @@ from topic_modeling.utils.corpus import preprocess
 with open('data/20ng.txt') as f:
     corpus = f.readlines()
 
-text = [preprocess(x) for x in tqdm(corpus)]
+text = [preprocess(x) + '\n' for x in tqdm(corpus)]
 
 with open('data/20ng.txt', 'w') as f:
     f.writelines(text)
